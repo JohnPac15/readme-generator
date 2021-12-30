ea0dfbb1beeb419a8b9607a233a0b05baffacb28
JohnPac <johnpac15@yahoo.com>
2021-12-30 15:50:54 -0600

    added the readme file

--- index.js
+++ index.js
@@ -8,84 +8,84 @@ const Choices = require("inquirer/lib/objects/choices");
 
 // TODO: Create an array of questions for user input
 const Questions = [
-    // {
-    //     type: 'input',
-    //     name: 'username',
-    //     message: 'What is your name? (Required)',
-    //     validate: nameInput => {
-    //         if (nameInput) {
-    //             return true
-    //         }
-    //         else {
-    //             console.log("please enter your name")
-    //             return false
-    //         }
-    //     }
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'email',
-    //     message: "Please enter your email ",
-    //     default: 'Johnpac15@yahoo.com'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'github',
-    //     message: "What is your Github username? ",
-    //     default: 'Johnpac15'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'repo',
-    //     message: "What is the name of your Github Repo? ",
-    //     default: 'readme-generator'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'title',
-    //     message: "What is the title of your project? ",
-    //     default: "Readme Generate"
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: "Please enter a discription of the project",
-    //     validate: projectTitle => {
-    //         if (projectTitle.length < 1) {
-    //             return console.log("A valid project discription required")
-    //         }
-    //         else {
-    //             return true
-    //         }
-    //     }
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'installation',
-    //     message: "Please enter the installation instructions"
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'usage',
-    //     message: "Please enter some usage examples of the project"
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'credit',
-    //     message: "Please list everyone who deserves credit for this project"
-    // },
-    // {
-    //     type: 'list',
-    //     name: 'license',
-    //     message: 'What ype of license would you like? ',
-    //     choices: ['Apache 2.0','Boost','BSD' ],
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            }
+            else {
+                console.log("please enter your name")
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter your email ",
+        default: 'Johnpac15@yahoo.com'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is your Github username? ",
+        default: 'Johnpac15'
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: "What is the name of your Github Repo? ",
+        default: 'readme-generator'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your project? ",
+        default: "Readme Generate"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please enter a discription of the project",
+        validate: projectTitle => {
+            if (projectTitle.length < 1) {
+                return console.log("A valid project discription required")
+            }
+            else {
+                return true
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Please enter the installation instructions"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Please enter some usage examples of the project"
+    },
+    {
+        type: 'input',
+        name: 'credit',
+        message: "Please list everyone who deserves credit for this project"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What ype of license would you like? ',
+        choices: ['Apache 2.0','Boost','BSD' ],
         
-    // },
-    // {
-    //     type: 'confirm',
-    //     name: 'contribution',
-    //     message: "would you like to add contribution guidelines? "
-    // },
+    },
+    {
+        type: 'confirm',
+        name: 'contribution',
+        message: "would you like to add contribution guidelines? "
+    },
     {
         type: 'input',
         name: 'testing',
@@ -95,7 +95,7 @@ const Questions = [
 
 const writeReadMe = (fileContent) => {
     return new Promise((resolve, reject) => {
-        fs.writeFile('./testing/README.md',fileContent, err => {
+        fs.writeFile('./utils/README.md',fileContent, err => {
             if(err){
                 reject(err)
                 return;
